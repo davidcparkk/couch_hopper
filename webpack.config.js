a960464e9270c04d55759bf9dfcd38b598476269
David Park <xdavidpark@gmail.com>
2019-11-04 20:56:15 -0800

    Add webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,22 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-    rules: {
-      test: [/\.jsx?$/],
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/env', '@babel/react']
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
         }
       }
-    }
+    ]
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.js', 'jsx.', '*']
+    extensions: ['.js', '.jsx', '*']
   },
 
 }
